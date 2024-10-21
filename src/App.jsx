e1139fc8a86a2f1bd629b78edcca37c80de5c675
MursalinDevOps <mursalin1768@gmail.com>
2024-10-21 21:32:06 +0600

    initial setup completed

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <header className='bg-black text-white text-center'>
+        <nav className='flex justify-between px-4 py-2'>
+        <h1 className='text-xl'>The Blog  Cafe</h1>
+        {/* <img src="" alt="" /> */}
+        <p>Person</p>
+        </nav>
+      </header>
+    </>
+  )
+}
+
+export default App
