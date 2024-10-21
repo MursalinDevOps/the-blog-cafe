ef721d9e7d2b29149a4b61eed337bf0dd02f26a3
MursalinDevOps <mursalin1768@gmail.com>
2024-10-21 22:08:35 +0600

    nav added

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,10 @@
 import './App.css'
+import Header from './components/Header/Header'
 
 function App() {
   return (
     <>
-      <header className='bg-black text-white text-center'>
-        <nav className='flex justify-between px-4 py-2'>
-        <h1 className='text-xl'>The Blog  Cafe</h1>
-        {/* <img src="" alt="" /> */}
-        <p>Person</p>
-        </nav>
-      </header>
+      <Header></Header>
     </>
   )
 }
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+export default function Header() {
+  return (
+    <div>
+        <header>
+        <nav className='flex justify-between px-4 py-2'>
+        <h1 className='text-xl'>The Blog  Cafe</h1>
+        </nav>
+      </header>
+    </div>
+  )
+}
